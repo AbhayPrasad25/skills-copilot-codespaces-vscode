083497e740d1f751d640829a8af54eb0c55fea5b
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2024-12-13 09:53:54 +0000

    Copilot Second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillMember() {
+  this.name = 'skillMember';
+  this.skill = 'skill';
+  this.skillMember = 'skillMember';
+  this.skillMember2 = 'skillMember2';
+  this.skillMember3 = 'skillMember3';
+}ber();
